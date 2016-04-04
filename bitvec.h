c5b6516917702a8ca1cdd6154de0c4942f05e595
Masatoshi Teruya <mah0x211@gmail.com>
2016-04-05 00:29:06 +0900

    add: bitvec_ffz API has been added.

--- bitvec.h
+++ bitvec.h
@@ -33,7 +33,8 @@
 #include <stdint.h>
 #include <string.h>
 
-#define BV_TYPE uint64_t
+#define BV_TYPE     uint64_t
+#define BV_TYPEMAX ((BV_TYPE)-1)
 
 static const uint8_t BV_BIT = CHAR_BIT * sizeof( BV_TYPE );
 
@@ -113,19 +114,29 @@ static inline void bitvec_dispose( bitvec_t *bv )
  *
  *  m-sequence(maximal length sequence)
  *
- *    0x0218A7A392DD9ABFUL
- *    0x02FCA8CF75A6C487UL
- *    0x03F566ED27179461UL
- *    0x03C953422DFAE33BUL
- *    0x03848D96BBCC54FDUL
- *    0x03731D7ED10B2A4FUL
+ *  8 bit
+ *    0x1D
+ *
+ *  16 bit
+ *    0x0F2D
+ *
+ *  32 bit
+ *    0x077CB531
+ *
+ *  64 bit
+ *    0x0218A7A392DD9ABF
+ *    0x02FCA8CF75A6C487
+ *    0x03F566ED27179461
+ *    0x03C953422DFAE33B
+ *    0x03848D96BBCC54FD
+ *    0x03731D7ED10B2A4F
  *
  *
- *  mapping table calculation
+ *  mapping table calculation (64 bit)
  *
- *    uint_fast8_t tbl[64] = {0};
- *    uint_fast64_t m = BIT_MLS;
- *    uint_fast8_t i = 0;
+ *    uint8_t tbl[64] = {0};
+ *    uint64_t m = BIT_MLS;
+ *    uint8_t i = 0;
  *
  *    for(; i < 64; i++ ){
  *    tbl[m >> 58] = i;
@@ -134,14 +145,29 @@ static inline void bitvec_dispose( bitvec_t *bv )
  *
  */
 
+#define BV_MLS8     UINT8_C(0x1D)
+#define BV_MLS16    UINT16_C(0x0F2D)
+
+#define BV_MLS32    UINT32_C(0x077CB531)
+
+static const uint8_t BV_NTZ32TBL[] = {
+    0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 31, 27, 13, 23,
+    21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+};
+
+
 #define BV_MLS64    UINT64_C(0x03F566ED27179461)
 
-static const uint_fast8_t BV_NTZ64TBL[64] = {
+static const uint8_t BV_NTZ64TBL[64] = {
     0, 1, 59, 2, 60, 40, 54, 3, 61, 32, 49, 41, 55, 19, 35, 4, 62, 52, 30, 33,
     50, 12, 14, 42, 56, 16, 27, 20, 36, 23, 44, 5, 63, 58, 39, 53, 31, 48, 18,
     34, 51, 29, 11, 13, 15, 26, 22, 43, 57, 38, 47, 17, 28, 10, 25, 21, 37, 46, 
     9, 24, 45, 8, 7, 6
 };
+
+
+#define BV_NTZ(v) \
+    BV_NTZ64TBL[(((v) & -(v)) * BV_MLS64) >> 58]
 
 
 static inline size_t bitvec_ntz( bitvec_t *bv )
@@ -152,12 +178,36 @@ static inline size_t bitvec_ntz( bitvec_t *bv )
     for(; idx < bv->nvec; idx++ )
     {
         if( vec[idx] ){
-            return ( BV_BIT * idx ) +
-                    BV_NTZ64TBL[( ( vec[idx] & -(vec[idx]) ) * BV_MLS64) >> 58];
+            return ( BV_BIT * idx ) + BV_NTZ( vec[idx] );
         }
     }
 
     return bv->nbit;
+}
+
+
+static inline size_t bitvec_ffz( bitvec_t *bv )
+{
+    BV_TYPE *vec = bv->vec;
+    size_t idx = 0;
+
+    if( !*vec ){
+        return 0;
+    }
+
+    for(; idx < bv->nvec; idx++ )
+    {
+        if( vec[idx] != BV_TYPEMAX )
+        {
+            idx = ( BV_BIT * idx ) + BV_NTZ( ~vec[idx] );
+            if( idx > bv->nbit ){
+                break;
+            }
+            return idx;
+        }
+    }
+
+    return SIZE_MAX;
 }
 
 
